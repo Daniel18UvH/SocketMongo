62bbb1c5d4d22ff046bdb40bbba110f4739d1d2f
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2024-03-25 14:20:25 -0600

    add 250324

--- public/js/script.js
+++ public/js/script.js
@@ -1,69 +1,66 @@
-const socket=io();
+const socket = io();
 var mensajeDiv = document.getElementById("mensaje");
 var datos = document.getElementById("datos");
-//MOSTRAR DATOS DE MONGODB
-socket.on("servidorEnviarUsuarios",(usuarios)=>{
-    var tr="";
-    usuarios.forEach((usuario,idLocal)=>{
-        tr=tr+`
-            <tr>
-                <td>${(idLocal+1)*100}</td>
-                <td>${usuario.nombre}</td>
-                <td>${usuario.usuario}</td>
-                <td>${usuario.password}</td>
-                <td>
-                    <a href="#" onclick="editarUsuario('${usuario._id}')">Editar</a> / 
-                    <a href="#" onclick="borrarUsuario('${usuario._id}')">Borrar</a>
-                </td>
-            </tr>
+
+//Mostrar datos de MongoDB de usuarios
+socket.on("servidorEnviarUsuarios", (usuarios) => {
+    var tr = "";
+    usuarios.forEach((usuario, idLocal) => {
+        tr = tr + `
+        <tr>
+            <td>${idLocal + 1}</td>
+            <td>${usuario.nombre}</td>
+            <td>${usuario.usuario}</td>
+            <td>${usuario.password}</td>
+            <td>
+                <a href="#" onClick="editarUsuario('${usuario._id}')">Editar</a>
+                <a href="#" onClick="borrarUsuario('${usuario._id}')">Borrar</a>
+            </td>
+        </tr>
         `;
     });
-    datos.innerHTML=tr;
+    datos.innerHTML = tr;
 });
 
-// GUARDAR DATOS A MONGODB
-var enviarDatos=document.getElementById("enviarDatos");
-enviarDatos.addEventListener("submit",(e)=>{
+//Guardar datos de MongoDB de usuarios
+var enviarDatos = document.getElementById("enviarDatos");
+enviarDatos.addEventListener("submit", (e) => {
     e.preventDefault();
-    //RECIBIR LOS DATOS DEL FORMULARIO
-    var usuario={
-        id:document.getElementById("id").value,
-        nombre:document.getElementById("nombre").value,
-        usuario:document.getElementById("usuario").value,
-        password:document.getElementById("password").value,
+    var usuario = {
+        nombre: document.getElementById("nombre").value,
+        usuario: document.getElementById("usuario").value,
+        password: document.getElementById("password").value
     }
-    socket.emit("clienteGuardarUsuario",usuario);
-    socket.on("servidorUsuarioGuardado",(mensaje)=>{
+    socket.emit("clienteGuardarUsuario", usuario);
+    socket.on("servidorUsuarioGuardado", (mensaje) => {
         console.log(mensaje);
-        mensajeDiv.innerHTML=mensaje;
-        setTimeout(()=>{
-            mensajeDiv.innerHTML="";
-        },3000);
-        //REINICIAR EL FORMULARIO
-        document.getElementById("nombre").value="";
-        document.getElementById("usuario").value="";
-        document.getElementById("password").value="";
-        document.getElementById("nombre").focus();
+        mensajeDiv.innerHTML = mensaje;
+        setTimeout(() => {
+            mensajeDiv.innerHTML = "";
+        }, 2000);
     });
 
+    document.getElementById("nombre").value = "";
+    document.getElementById("usuario").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("nombre").focus()
+
 });
 
-// MODIFICAR UN REGISTRO DE MONGODB
-function editarUsuario(id){
-    console.log(id);
-    socket.emit("clienteObtenerUsuarioPorID",id);
+
+//Modificar un registro de MongoDB de usuarios
+function editarUsuario(id) {
+    window.location.href = 'editarUsuario.html?id=' + id;
 }
-socket.on("servidorObtenerUsuarioPorID",(usuario)=>{
-    console.log(usuario);
-    document.getElementById("id").value=usuario._id;
-    document.getElementById("nombre").value=usuario.nombre;
-    document.getElementById("usuario").value=usuario.usuario;
-    document.getElementById("password").value=usuario.password;
-    document.getElementById("txtNuevoUsuario").innerHTML="Editar usuario";
-    document.getElementById("txtGuardarUsuario").innerHTML="Guardar cambios";
-});
-// ELIMINAR UN REGISTRO DE MONGODB
-function borrarUsuario(id){
-    console.log(id);
+//Eliminar un registro de MongoDB de usuarios
+function borrarUsuario(id) {
     socket.emit("clienteBorrarUsuario", id);
+    socket.on("servidorUsuarioBorrado", (mensaje) => {
+        mensajeDiv.innerHTML = mensaje;
+        setTimeout(() => {
+            mensajeDiv.innerHTML = "";
+            location.reload();
+        }, 1000);
+    })
 }
+
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,45 +1,131 @@
-const Usuario=require("../modelos/usuario");
-function socket(io){
-    io.on("connection",(socket)=>{
+const Usuario = require("../models/usuario");
+const Producto = require("../models/productos");
+
+function socket(io) {
+    io.on("connection", (socket) => {
         //MOSTRAR USUARIOS
         mostrarUsuarios();
-        async function mostrarUsuarios(){
-            const usuarios=await Usuario.find();
+        async function mostrarUsuarios() {
+            const usuarios = await Usuario.find();
             io.emit("servidorEnviarUsuarios", usuarios);
         }
-        
-        //GUARDAR USUARIO
-        socket.on("clienteGuardarUsuario", async(usuario)=>{
-            try{
-                if(usuario.id==""){
-                    await new Usuario(usuario).save();
-                    io.emit("servidorUsuarioGuardado","Usuario guardado");
-                }
-                else{
-                    await Usuario.findByIdAndUpdate(usuario.id,usuario);
-                    io.emit("servidorUsuarioGuardado","Usuario modificado");
-                }
-                mostrarUsuarios();
 
-            }
-            catch(err){
-                console.log("Error al registrar al usuario "+err);
+        //Guardar usuario
+        socket.on("clienteGuardarUsuario", async (usuario) => {
+            try {
+                await new Usuario(usuario).save();
+                io.emit("servidorUsuarioGuardado", "Usuario guardado");
+                console.log("Usuario guardado");
+            } catch (error) {
+                console.log(error);
             }
         });
 
-        //OBTENER USUARIO POR ID
-        socket.on("clienteObtenerUsuarioPorID",async(id)=>{
-            const usuario=await Usuario.findById(id);
-            io.emit("servidorObtenerUsuarioPorID",usuario);
+        // Editar usuario 
+        socket.on("clienteEditarUsuarioId", async (id) => {
+            try {
+                const usuario = await Usuario.findById(id);
+                io.emit("servidorUsuarioEditadoId", usuario);
+            } catch (err) {
+                console.error("Error al editar usuario");
+            }
         });
 
-        //BORRAR USUARIO POR ID
-        socket.on("clienteBorrarUsuario",async(id)=>{
-            await Usuario.findByIdAndDelete(id);
-            io.emit("servidorUsuarioGuardado","Usuario borrado");
-            mostrarUsuarios();
+        // Editar usuario
+        socket.on("clienteEditarUsuario", async (usuarioEditado) => {
+            try {
+                const usuarioActualizado = await Usuario.findByIdAndUpdate(
+                    usuarioEditado._id,
+                    {
+                        nombre: usuarioEditado.nombre,
+                        usuario: usuarioEditado.usuario,
+                        password: usuarioEditado.password,
+                    },
+                    { new: true }
+                ); // Para obtener el documento actualizado
+
+                io.emit("servidorUsuarioEditado", usuarioActualizado);
+            } catch (err) {
+                console.error("Error al editar usuario:", err);
+            }
         });
 
-    });//FIN IO.ON
+        // Borrar usuario
+        socket.on("clienteBorrarUsuario", async (id) => {
+            try {
+                await Usuario.findByIdAndDelete(id);
+                io.emit("servidorUsuarioBorrado", "Usuario borrado");
+            } catch (err) {
+                console.error("Error al borrar usuario");
+            }
+        });
+
+        // Mostrar Productos
+        mostrarProductos();
+        async function mostrarProductos() {
+            try {
+                const productos = await Producto.find();
+                io.emit("servidorEnviarProductos", productos);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        // Guardar Productos
+        socket.on("clienteGuardarPro", async (producto) => {
+            try {
+                await new Producto(producto).save();
+                io.emit("servidorProductoGuardado", "Producto guardado");
+                console.log("Producto guardado");
+            } catch (error) {
+                console.log(error);
+            }
+        });
+
+        // Editar producto
+        socket.on("clienteEditarProducto", async (id) => {
+            try {
+                const producto = await Producto.findById(id);
+                io.emit("servidorProductoEditado", producto);
+            } catch (err) {
+                console.error("Error al editar producto");
+            }
+        });
+
+        // Editar producto
+        socket.on("clienteEditarProducto", async (productoEditado) => {
+            try {
+                const productoActualizado = await Producto.findByIdAndUpdate(
+                    productoEditado._id,
+                    {
+                        id: productoEditado.id,
+                        nombre: productoEditado.nombre,
+                        precio: productoEditado.precio,
+                        cantidad: productoEditado.cantidad,
+                    },
+                    { new: true }
+                );
+
+                io.emit("servidorProductoEditado", productoActualizado);
+            } catch (err) {
+                console.error("Error al editar producto:", err);
+            }
+        });
+
+        // Borrar Producto
+        socket.on("clienteBorrarProducto", async (id) => {
+            try {
+                await Producto.findByIdAndDelete(id);
+                io.emit("servidorProductoBorrado", "Producto borrado");
+                mostrarProductos();
+            } catch (err) {
+                console.error("Error al borrar producto");
+            }
+        });
+    });//Fin de io.on("connection")
 }
-module.exports=socket;
+
+
+module.exports = socket;
+
+
