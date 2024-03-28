2b14c268ba54ac64b06423f1e9ffcf9fcc8bd9aa
Daniel18UvH <ulisesvh.ti22@utsjr.edu.mx>
2024-03-27 21:54:21 -0600

    add270324

--- models/usuario.js
+++ models/usuario.js
@@ -1,21 +1,22 @@
-const {mongoose} =require("../database/conexion");
+const { mongoose } = require('../database/conexion');
 const usuarioSchema = new mongoose.Schema({
-    nombre:{
-        type:String,
-        require:true
+    nombre: {
+        type: String,
+        required: true
     },
-    usuario:{
-        type:String,
-        require:true
+    usuario: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        require:true
+    password: {
+        type: String,
+        required: true
     },
-    estatus:{
-        type:Boolean,
-        default:true
+    status: {
+        type: Boolean,
+        default: true
     }
 });
 
-module.exports=mongoose.model("usuario",usuarioSchema);
+
+module.exports = mongoose.model('usuario', usuarioSchema);
